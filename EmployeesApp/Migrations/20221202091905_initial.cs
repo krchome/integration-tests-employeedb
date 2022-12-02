5f31bfcd497327205dcdf306b6780f376136b5ec
krchome <krchome@hotmail.com>
2022-12-02 22:41:02 +1300

    first commit

---
+++ EmployeesApp/Migrations/20221202091905_initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeesApp.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    AccountNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employee",
+                columns: new[] { "Id", "AccountNumber", "Age", "Name" },
+                values: new object[] { new Guid("43ee0732-74aa-4550-81ef-d46616c0b698"), "123-3452134543-32", 30, "Mark" });
+
+            migrationBuilder.InsertData(
+                table: "Employee",
+                columns: new[] { "Id", "AccountNumber", "Age", "Name" },
+                values: new object[] { new Guid("aae6883e-5ddf-4c62-911a-852ffa9a87ac"), "123-9384613085-55", 28, "Evelin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
